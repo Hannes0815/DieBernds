aaa9e54c2748263884b9f29ed7167041e68128ef
PalatinCoder <github@jan-sl.de>
2017-06-09 13:06:42 +0200

    TASK: Implement security checks
    The security controller intercepts every request and checks if the user provided a valid token. The result is stored in the res object for further use in the controllers, as they must decide how to handle the request accordingly.

--- controller/security.js
+++ controller/security.js
@@ -1,4 +1,20 @@
+var jwt = require('jsonwebtoken');
+
 module.exports = function(req, res, next) {
-  console.log('security check executed');
+  // if no token was provided or could not be verified, the request is not authenticated
+  res.locals.authenticated = false;
+  // look for the token
+  var token = req.headers['x-bernd-token'];
+  if (token) {
+    // secret should not be hardcoded nor should it be commited to public vcs in cleartext, but wayne :D
+    try {
+      var decodedJwt = jwt.verify(token, 'BerndsGeheimesSignaturSecret!!11');
+      res.locals.authenticated = true;
+      res.locals.token = decodedJwt;
+    } catch (e) {
+      // noop
+      // If the token could not be verified, handle gracefully and just keep the request unauthenticated
+    }
+  }
   next();
 }
