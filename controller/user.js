0d79977bbb6051d1344f4d95d8340fdbb881d5dc
PalatinCoder <github@jan-sl.de>
2017-06-12 11:19:59 +0200

    FEATURE: Implement user login

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,21 @@
+var jwt = require('jsonwebtoken');
+
 exports.login = function(req, res) {
-  res.send('login');
+  var user = require('../data/user.json');
+
+  if (req.body.username != user.username || req.body.password != user.password) {
+    res.status(403).json({
+      message: 'Username or password is incorrect'
+    });
+    return;
+  }
+
+  // generate token
+  var token = jwt.sign({username: user.username}, 'BerndsGeheimesSignaturSecret!!11');
+
+  res.status(200).json({
+    token: token
+  });
 }
 
 exports.changePassword = function(req, res) {
