f5d9be8f925dd70811cf679527a8403f81ab37c5
PalatinCoder <github@jan-sl.de>
2017-06-12 11:40:33 +0200

    FEATURE: Password can be changed

--- controller/user.js
+++ controller/user.js
@@ -19,5 +19,30 @@ exports.login = function(req, res) {
 }
 
 exports.changePassword = function(req, res) {
-  res.send('change pw');
+  if (!res.locals.authenticated) {
+    res.status(403).send();
+    return;
+  }
+
+  var user = require('../data/user.json');
+
+  if (req.body.password != user.password) {
+    res.status(403).json({
+      message: 'Password is incorrect'
+    });
+    return;
+  }
+
+  user.password = req.body.newPassword;
+
+
+  var fs = require('fs');
+
+  fs.writeFile('./data/user.json', JSON.stringify(user), 'utf-8', (err) => {
+    if (err) {
+      res.status(500).json({error: err});
+    } else {
+      res.status(200).send();
+    }
+  });
 }
