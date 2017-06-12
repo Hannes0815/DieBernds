4cf771bc4b07a6a947af03fa03c8b2c7c0329821
PalatinCoder <github@jan-sl.de>
2017-06-12 11:19:14 +0200

    TASK: Require and use body-parser

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var bodyparser = require('body-parser');
 var app = express();
 
 var user = require('./routes/user');
 var blog = require('./routes/blog');
 var security = require('./controller/security');
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.use('/api/V1', security);
 app.use('/api/V1', user);
